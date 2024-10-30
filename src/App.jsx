f0a83cb23adce5a7d8fef905f4488beda3557335
Christoffer Flodin <flodin.christoffer@gmail.com>
2024-10-30 09:49:11 +0100

    project started added daisyUi

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Sidebar  from "./components/Sidebar"
+import HomePage from "./pages/HomePage"
+
+function App() {
+
+  return (
+    <div className="flex">
+   <Sidebar />
+   <HomePage />
+    </div>
+  )
+}
+
+export default App
